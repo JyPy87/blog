185d25717f639669c3febcc2f9337f17c131df1d
JyPy87 <jeremy.pigny@outlook.fr>
2021-07-28 21:38:13 +0200

    add fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use Nelmio\Alice\Loader\NativeLoader;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,9 +10,14 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $loader = new NativeLoader();
 
+       //take fixtures
+        $entities = $loader->loadFile(__DIR__.'/fixtures.yml')->getObjects();
+        
+        foreach ($entities as $entity) {
+            $manager->persist($entity);
+        };
         $manager->flush();
     }
 }
