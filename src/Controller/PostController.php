f2a1f4fbb810f5890ec5c0634494d0323c6efcc7
JyPy87 <jeremy.pigny@outlook.fr>
2021-08-07 22:35:42 +0200

    add theme and main page

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,12 +10,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class PostController extends AbstractController
 {
     /**
-     * @Route("/post", name="post")
+     * @Route("/", name="post")
      */
-    public function index(): Response
+    public function index(PostRepository $postRepository): Response
     {
+        $posts = $postRepository->findAll();
         return $this->render('post/index.html.twig', [
-            'controller_name' => 'PostController',
+            'posts'=>$posts,
         ]);
     }
 }
