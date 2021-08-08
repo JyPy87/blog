1ec680579cddee1bac5cd96bfcc04036d04a0f20
JyPy87 <jeremy.pigny@outlook.fr>
2021-08-08 22:10:54 +0200

    add active on menu item

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -7,10 +7,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/", name="post_")
+ */
 class PostController extends AbstractController
 {
     /**
-     * @Route("/", name="post")
+     * @Route("/", name="main")
      */
     public function index(PostRepository $postRepository): Response
     {
