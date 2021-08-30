6bdc9e26806d80665626d7012015773540a23fb6
JyPy87 <jeremy.pigny@outlook.fr>
2021-08-30 16:14:27 +0200

    add main controller, modify article display

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -3,10 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\Post;
-use App\Form\SearchType;
 use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,7 +14,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class PostController extends AbstractController
 {
     /**
-     * @Route("", name="main")
+     * @Route("", name="index")
      */
     public function index(PostRepository $postRepository): Response
     {
@@ -33,5 +31,18 @@ class PostController extends AbstractController
         return $this->render('post/read.html.twig', [
             'post' => $post
         ]);
+    }
+    /**
+     * @Route("{cat}", name="article")
+     */
+    public function article(PostRepository $postRepository, string $cat)
+    {
+        $posts = $postRepository->findByCategory($cat);
+
+        return $this->render('post/index.html.twig',
+       [
+           'posts'=>$posts,        
+        ]);
+
     }
 }
